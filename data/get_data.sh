4674aab0041546a08253925336dde76955038741
Ignacio Ramirez Paulino <nacho@fing.edu.uy>
2019-12-06 16:56:59 -0300

    sdfsdoiu

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,5 +1,3 @@
 #!/bin/bash 
-wget -c http://iie.fing.edu.uy/~nacho/data/energia/darset_1.zip
-unzip -f darset_1.zip
-wget -c http://iie.fing.edu.uy/~nacho/data/energia/darset_2.zip
+wget -c http://iie.fing.edu.uy/~nacho/data/energia/darset-20191206.zip
 unzip -f darset_2.zip
