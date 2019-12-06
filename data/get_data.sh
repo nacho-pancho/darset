784230c82023ab6e78cb87967b3d433bf1950438
Ignacio Ramirez Paulino <nacho@fing.edu.uy>
2019-12-06 16:25:49 -0300

    pepe

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,3 +1,5 @@
 #!/bin/bash 
 wget -c http://iie.fing.edu.uy/~nacho/data/energia/darset_1.zip
-unzip darset_1.zip
+unzip -f darset_1.zip
+wget -c http://iie.fing.edu.uy/~nacho/data/energia/darset_2.zip
+unzip -f darset_2.zip
