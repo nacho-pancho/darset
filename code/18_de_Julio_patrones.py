4ad20dd2900001a1b674879e3a4e11b876aefde8
Felipe Palacio <fpalacio@mercado.com.uy>
2020-10-19 11:05:14 -0300

    --

--- code/18_de_Julio_patrones.py
+++ code/18_de_Julio_patrones.py
@@ -39,12 +39,16 @@ if __name__ == '__main__':
     #nom_series_p1 = ['velGEN','dirGEN','velPRONOS','dirPRONOS','potSCADA']
     nom_series_p1 = ['velxSCADA','velySCADA']
     nom_series_p1 = [s + '_' + str(nid_p1) for s in nom_series_p1]
+    '''
     vel_GEN_p1 = parque1.medidores[0].get_medida('vel','gen')
     dir_GEN_p1 = parque1.medidores[0].get_medida('dir','gen')
+    '''
     vel_scada_p1 = parque1.medidores[0].get_medida('vel','scada')
     dir_scada_p1 = parque1.medidores[0].get_medida('dir','scada')
+    '''
     vel_pronos_p1 = parque1.medidores[0].get_medida('vel','pronos')
     dir_pronos_p1 = parque1.medidores[0].get_medida('dir','pronos')
+    '''
     meds_plot_p1 = [vel_scada_p1, dir_scada_p1]
 
     # lectura de los datos del parque2 al cual se le van a calcular las RO.
@@ -52,8 +56,8 @@ if __name__ == '__main__':
     nid_p2 = 57
     parque2 = archivos.leerArchivosCentral(nid_p2)
     
-    tini = datetime.datetime(2020, 4, 1)  
-    tfin = datetime.datetime(2020, 6, 1)
+    tini = datetime.datetime(2020, 8, 1)  
+    tfin = datetime.datetime(2020, 9, 1)
     archi = archivos.archi_ro_pendientes(nid_p2)
     parque2.calcular_liq_pendientes(tini, tfin, archi)
     
@@ -66,10 +70,10 @@ if __name__ == '__main__':
     nom_series_p2 = ['potSCADA']
     nom_series_p2 = [s + '_' + str(nid_p2) for s in nom_series_p2]
     
-    vel_PRONOS_p2 = parque2.medidores[0].get_medida('vel','pronos')
+    #vel_PRONOS_p2 = parque2.medidores[0].get_medida('vel','pronos')
     #vel_GEN_p2 = parque2.medidores[0].get_medida('vel','gen')
     #vel_SCADA_p2 = parque2.medidores[0].get_medida('vel','scada')
-    dir_PRONOS_p2 = parque2.medidores[0].get_medida('dir','pronos')
+    #dir_PRONOS_p2 = parque2.medidores[0].get_medida('dir','pronos')
     meds_plot_p2 = [parque2.pot, parque2.cgm]
 
     dt_ini_calc, dt_fin_calc = archivos.leer_ro_pendientes(parque2.id)
